c490eda66c0e93b7ac0cc91646522d6375df46a4
Dmytro Litvinov <dmlitvinov99@gmail.com>
2025-12-22 01:26:22 -0500

    added two more tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,58 +1,34 @@
 import responses
 
 from api.config import REQRES_BASE_URL
-from api.models.user_models import User
-from api.models.user_models import SingleUserResponse, ListUsersResponse
+from api.models.auth_models import LoginSuccessResponse, ErrorResponse
 
 @responses.activate
-def test_get_user_mock(reqres_client):
+def test_login_success_mock(reqres_client):
     responses.add(
-        method=responses.GET,
-        url=f"{REQRES_BASE_URL}/api/users/2",
-        json={
-            "data": {
-                "id": 2,
-                "email": "janet.weaver@reqres.in",
-                "first_name": "Janet",
-                "last_name": "Weaver",
-                "avatar": "https://reqres.in/img/faces/2-image.jpg",
-            }
-        },
+        method=responses.POST,
+        url=f"{REQRES_BASE_URL}/api/login",
+        json={"token": "fake-token-123"},
         status=200,
         content_type="application/json",
     )
 
-    resp = reqres_client.get_user(user_id=2)
-
+    resp = reqres_client.login(email="eve.holt@reqres.in", password="cityslicka")
     assert resp.status_code == 200
-    payload = SingleUserResponse.model_validate(resp.json)
-    assert payload.data.id == 2
-
+    model = LoginSuccessResponse.model_validate(resp.json)
+    assert model.token == "fake-token-123"
 
 @responses.activate
-def test_list_users_mock(reqres_client):
+def test_login_missing_password_mock(reqres_client):
     responses.add(
-        method=responses.GET,
-        url=f"{REQRES_BASE_URL}/api/users?page=1",
-        json={
-            "page": 1,
-            "data": [
-                {
-                    "id": 1,
-                    "email": "george.bluth@reqres.in",
-                    "first_name": "George",
-                    "last_name": "Bluth",
-                    "avatar": "https://reqres.in/img/faces/1-image.jpg",
-                }
-            ],
-        },
-        status=200,
+        method=responses.POST,
+        url=f"{REQRES_BASE_URL}/api/login",
+        json={"error": "Missing password"},
+        status=400,
         content_type="application/json",
     )
-
-    resp = reqres_client.list_users(page=1)
-
-    assert resp.status_code == 200
-    payload = ListUsersResponse.model_validate(resp.json)
-    assert payload.page == 1
-    assert len(payload.data) == 1
+    
+    resp = reqres_client.login_missing_password(email="peter@klaven")
+    assert resp.status_code == 400
+    model = ErrorResponse.model_validate(resp.json)
+    assert model.error == "Missing password"
---
+++ tests/test_users.py
@@ -0,0 +1,57 @@
+import responses
+
+from api.config import REQRES_BASE_URL
+from api.models.user_models import SingleUserResponse, ListUsersResponse
+
+@responses.activate
+def test_get_user_mock(reqres_client):
+    responses.add(
+        method=responses.GET,
+        url=f"{REQRES_BASE_URL}/api/users/2",
+        json={
+            "data": {
+                "id": 2,
+                "email": "janet.weaver@reqres.in",
+                "first_name": "Janet",
+                "last_name": "Weaver",
+                "avatar": "https://reqres.in/img/faces/2-image.jpg",
+            }
+        },
+        status=200,
+        content_type="application/json",
+    )
+
+    resp = reqres_client.get_user(user_id=2)
+
+    assert resp.status_code == 200
+    payload = SingleUserResponse.model_validate(resp.json)
+    assert payload.data.id == 2
+
+
+@responses.activate
+def test_list_users_mock(reqres_client):
+    responses.add(
+        method=responses.GET,
+        url=f"{REQRES_BASE_URL}/api/users?page=1",
+        json={
+            "page": 1,
+            "data": [
+                {
+                    "id": 1,
+                    "email": "george.bluth@reqres.in",
+                    "first_name": "George",
+                    "last_name": "Bluth",
+                    "avatar": "https://reqres.in/img/faces/1-image.jpg",
+                }
+            ],
+        },
+        status=200,
+        content_type="application/json",
+    )
+
+    resp = reqres_client.list_users(page=1)
+
+    assert resp.status_code == 200
+    payload = ListUsersResponse.model_validate(resp.json)
+    assert payload.page == 1
+    assert len(payload.data) == 1
